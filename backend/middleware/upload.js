6b2f9d3086a2d5a25bd926b38be80dd753fe6802
Zuhair Ahmed <153817334+Zuhair0000@users.noreply.github.com>
2026-07-14 21:18:09 +0800

    Fix: added limitation to the image size as well as image type

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -11,6 +11,17 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fieldSize: 5 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    const allowed = ["image/jpeg", "image/png", "image/webp"];
+    if (allowed.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Erro("Only JPEG, PNG, and WEBP images are allowed"));
+    }
+  },
+});
 
 module.exports = upload;
